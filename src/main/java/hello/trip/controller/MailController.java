758106943ad54a14a9c7e02b070f0c027eae8120
useop <useop0821@gmail.com>
2024-09-16 19:17:27 +0900

    #이메일 인증

---
+++ src/main/java/hello/trip/controller/MailController.java
@@ -0,0 +1,25 @@
+package hello.trip.controller;
+
+import hello.trip.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+    @ResponseBody
+    @PostMapping("/mail")
+    public String MailSend(String mail){
+
+        int number = mailService.sendMail(mail);
+
+        String num = "" + number;
+
+        return num;
+    }
+}
