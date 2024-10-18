4da64655bdefba2859cff78493c868444d1a12bc
useop <useop0821@gmail.com>
2024-10-19 00:14:37 +0900

    #3

--- src/main/java/hello/trip/controller/MainController.java
+++ src/main/java/hello/trip/controller/MainController.java
@@ -15,15 +15,15 @@ public class MainController {
     private final UserService userService;
 
     @PostMapping("/")
-    public String login(@RequestParam("username") String username,
-                        @RequestParam("password") String password,
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
                         Model model) {
 
         if (userService.validateUser(username, password)) {
-            return "redirect:/map"; // 로그인 성공 시 map 페이지로 이동
+            return "redirect:/map";
         } else {
             model.addAttribute("loginError", "아이디 비밀번호를 확인하세요");
-            return "index"; // 로그인 실패 시 index 페이지로 다시 이동
+            return "index";
         }
     }
 
